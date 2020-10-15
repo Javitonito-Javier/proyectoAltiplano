215b7f3b38246680be03908ded4c51cf1df90138
Javitonito-Javier <jkmeza92@gmail.com>
2020-10-15 12:27:03 -0700

    test1

---
+++ Proyecto Altiplano/Proyecto Altiplano/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, Agregar.Click
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        dsd
+    End Sub
+End Class
